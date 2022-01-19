a189feb9c141ce55f2c9679bb9b14e32b8b3b4f2
Moi <92033476+Caojerem@users.noreply.github.com>
2022-01-19 16:58:10 +0100

    Handling shx missing error
    Create shx file from shp and dfb file.

--- src/load_shp.py
+++ src/load_shp.py
@@ -1,13 +1,21 @@
-import shapefile
 from geopandas import read_file
+from shapefile import (Reader, Writer)
+from io import BytesIO as IO
 
 # Path to the Shapefile
-shapefile_name = 'HAIE.shp'
-
-# Display the element of the Shapefile
-# r = shapefile.Reader(shapefile_name)
+shapefile_name = "HAIE"
 
 # Display the shapefile in GeoJson format
-f = read_file(shapefile_name)
+try:
+    f = read_file(shapefile_name+'.shp')
+except :
+    with IO() as shpio, IO() as dbfio:  # Don't overwrite existing .shp, .dbf
+        with Reader(shapefile_name) as r, Writer(shp=shpio, dbf=dbfio, shx=shapefile_name+'.shx') as w:
+            w.fields = r.fields[1:]  # skip first deletion field
+            for rec in r.iterShapeRecords():
+                w.record(*rec.record)
+                w.shape(rec.shape)
+    w.close()
+    f = read_file(shapefile_name+'.shp')
 
 #print(f)
